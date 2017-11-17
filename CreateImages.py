9d65294fd3846f731660c5bb4cb7a975adfb6f47
GopalKolli <gkolli@asu.edu>
2017-11-17 08:38:27 -0700

    Image Generation (Data Synthesis) Complete. Little tweaking may be required.

--- CreateImages.py
+++ CreateImages.py
@@ -25,7 +25,7 @@ class ImageOperations:
         sec_width = width/number_of_sections
         ls = []
         for i in range(number_of_sections+1):
-            ls.append(belt_start + (sec_width*i))
+            ls.append(int(belt_start + (sec_width*i)))
         return ls
 
 
@@ -52,9 +52,139 @@ class ImageOperations:
             else:
                 panel = panel + 1
 
-        template.show()
+        #template.show()
+        return template
+
+
+    def pasteNoiseOnImage(self,template, sectionsList, portion):
+
+        object = ""
+        panel = 1
+
+        while panel <= 5:
+            place_random  = np.random.uniform(low=0.0,high=1.0)
+            if place_random > 0.4:
+                object_random = np.random.uniform(low=0.0, high=1.0)
+                if object_random < 0.4:
+                    object = "H"
+                elif object_random >=0.4 and object_random < 0.7:
+                    object = "T"
+                else:
+                    object = "C"
+            else:
+                panel = panel + 1
+                continue
+
+
+            if object == "H":
+                h_margin = 48
+                w_margin = 64
+                maxsize = (64,48)
+                facing_random = np.random.uniform(low=0.0, high=1.0)
+                if facing_random > 0.4:
+                    if panel == 1:
+                        if portion == "Top":
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_Front_L2.jpg")
+                        else:
+                            noise_image = Image.open("ImageLab/images/N-Human_Bottom_Front-L2.jpg")
+                    elif panel == 2:
+                        if portion == "Top":
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_Front-L1.jpg")
+                        else:
+                            noise_image = Image.open("ImageLab/images/N-Human_Botton_Front-L1.jpg")
+                    elif panel == 3:
+                        if portion == "Top":
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_Front-Center.jpg")
+                        else:
+                            noise_image = Image.open("ImageLab/images/N-Human_Bottom_Front-center.jpg")
+                    elif panel == 4:
+                        if portion == "Top":
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_Front-R1.jpg")
+                        else:
+                            noise_image = Image.open("ImageLab/images/N-Human_Bottom_Front-R1.jpg")
+                    else:
+                        if portion == "Top":
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_Front-R2.jpg")
+                        else:
+                            noise_image = Image.open("ImageLab/images/N-Human_Bottom_Front-R2.jpg")
+
+                else:
+                    if portion == "Top":
+                        if panel == 1:
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_side-L2.jpg")
+                        elif panel == 2:
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_side-L1.jpg")
+                        elif panel == 3:
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_side-center.jpg")
+                        elif panel == 4:
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_side-R1.jpg")
+                        else:
+                            noise_image = Image.open("ImageLab/images/N-Human_Top_side-R2.jpg")
+                    else:
+                        panel = panel + 1
+                        continue
+
+            elif object == "T":
+                h_margin = 48
+                w_margin = 64
+                maxsize = (64, 48)
+                if panel == 1:
+                    noise_image = Image.open("ImageLab/images/Tables-L2-edited.jpg")
+                elif panel == 2:
+                    noise_image = Image.open("ImageLab/images/Tables-L1-edited.jpg")
+                elif panel == 3:
+                    noise_image = Image.open("ImageLab/images/Tables-Center.jpg")
+                elif panel == 4:
+                    noise_image = Image.open("ImageLab/images/Tables-R1-edited.jpg")
+                else:
+                    noise_image = Image.open("ImageLab/images/Tables-R2-edited.jpg")
+
+            elif object == "C":
+                h_margin = 8
+                w_margin = 16
+                maxsize = (16,8)
+                if panel == 1:
+                    noise_image = Image.open("ImageLab/images/N-ciderBlock-Left.jpg")
+                elif panel == 2:
+                    noise_image = Image.open("ImageLab/images/N-ciderBlock-Left.jpg")
+                elif panel == 3:
+                    noise_image = Image.open("ImageLab/images/N-ciderBlocks-Center.jpg")
+                elif panel == 4:
+                    noise_image = Image.open("ImageLab/images/N-ciderBlocks-Right.jpg")
+                else:
+                    noise_image = Image.open("ImageLab/images/N-ciderBlocks-Right.jpg")
+
+            else:
+                panel = panel + 1
+                continue
+
+
+            if portion == "Top":
+                y_min = 226 - 80
+                y_max = 224 - h_margin
+            else:
+                y_min = 300
+                y_max = 380 - h_margin
+
+            x_min = sectionsList[panel - 1]
+            x_max = sectionsList[panel] - w_margin
+
+            x = np.random.uniform(low=x_min, high=x_max)
+            y = np.random.uniform(low=y_min, high=y_max)
+
+            x = int(x)
+            y = int(y)
+
+            noise_image.thumbnail(maxsize, Image.ANTIALIAS)
+            template.paste(noise_image, (x, y))
+            panel = panel + 1
 
         return template
+
+
+
+
+
 
 
     def pasteObjectOnBelt(self,current_Template, size, sectionsList, panelNumber):
--- GenerateDataset.py
+++ GenerateDataset.py
@@ -2,8 +2,18 @@ from CreateImages import ImageOperations
 
 operations = ImageOperations()
 
-template = operations.getTemplateReady()
 
-sectionsList = operations.divideIntoSections(belt_start=16, belt_end=496, number_of_sections=16)
 
-operations.generateImage(template, sectionsList)
+for i in range(100):
+    template = operations.getTemplateReady()
+
+    sectionsList = operations.divideIntoSections(belt_start=16, belt_end=496, number_of_sections=16)
+    Boxes_Image = operations.generateImage(template, sectionsList)
+
+    sectionsList = operations.divideIntoSections(belt_start=16, belt_end=496, number_of_sections=5)
+    NoiseImageTop = operations.pasteNoiseOnImage(Boxes_Image, sectionsList, "Top")
+    NoiseImageBottom = operations.pasteNoiseOnImage(NoiseImageTop, sectionsList, "Bottom")
+
+    generatedImage = NoiseImageBottom
+
+    generatedImage.save("results/img_" + str(i) + ".jpg", "JPEG")
